abd2425481adaf2c8d26c6ad6e42cee4a00e6500
obasola <dthompson98@gmail.com>
2016-06-18 15:14:46 -0500

    Replaced ngRoute with ui-router and all is working

--- WebContent/app/js/app.js
+++ WebContent/app/js/app.js
@@ -10,7 +10,8 @@ if (typeof String.prototype.endsWith !== 'function') {
 // Declare app level module which depends on filters, and services
 var myApp = angular.module('myApp', [
    'ngRoute'
-  ,'ngResource' 
+  ,'ui.router'
+  ,'ngResource'
   ,'ngCookies'
   ,'ngMaterial'
   ,'i18n'
@@ -38,11 +39,96 @@ var myApp = angular.module('myApp', [
   ,'team.module'
   ,'teamStat.module'
 ]);
-
+myApp.controller('appController',['$scope', '$state', function($scope,$state) {
+  $scope.showHomePage = function() {
+    $state.go('home');
+  };
+  $scope.showTeamHomePage = function(teamName) {
+    
+    $state.go('team');
+  };
+  $scope.showConference= function() {
+	    
+	    $state.go('conference');
+	  };
+  $scope.showProspects = function(teamName) {
+    
+    $state.go('player');
+  };
+  $scope.showMockRound = function(roundNumber) {
+    
+    $state.go('draftRound');
+  };
+  $scope.showDraftRound = function(roundNumber) {
+    $state.go('draftRound');
+  };
+}]);
 /**
  * Main configuration
  */
-myApp.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {templateUrl: 'partials/welcome.html'});
-  $routeProvider.otherwise({redirectTo: '/'});
+myApp.config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+  $stateProvider.state('home',{
+    url:'/'
+  })
+  .state('conference', {
+    url: '/conference',
+    templateUrl: 'partials/conference/conference_list.html'
+  })
+  .state('draft', {
+    url: '/draft',
+    templateUrl: 'partials/draft/draft_list.html'
+  })
+  .state('draftRound', {
+    url: '/draftRound',
+    templateUrl: 'partials/draftround/draftround_list.html'
+  })
+  .state('draftTeam', {
+    url: '/draftTeam',
+    templateUrl: 'partials/draftteam/draftteam_list.html'
+  })
+  .state('draftType', {
+    url: '/draftType',
+    templateUrl: 'partials/drafttype/drafttype_list.html'
+  })
+  .state('division', {
+    url: '/division',
+    templateUrl: 'partials/division/division_list.html'
+  })
+  .state('player', {
+    url: '/player',
+    templateUrl: 'partials/player/player_list.html'
+  })
+  .state('playerPosition', {
+    url: '/playerPosition',
+    templateUrl: 'partials/playerposition/playerposition_list.html'
+  })
+  .state('playerStat', {
+    url: '/playerStat',
+    templateUrl: 'partials/playerstat/playerstat_list.html'
+  })
+  .state('playerStatus', {
+    url: '/playerStatus',
+    templateUrl: 'partials/playerstatus/playerstatus_list.html'
+  })
+  .state('schedule', {
+    url:'/schedule',
+    templateUrl:'partials/schedule/schedule_list.html'
+  })
+  .state('scheduleType', {
+    url:'/scheduleType',
+    templateUrl:'partials/scheduletype/scheduletype_list.html'
+  })
+  .state('status', {
+    url:'/status',
+    templateUrl:'partials/status/status_list.html'
+  })
+  .state('team', {
+    url:'/team',
+    templateUrl:'partials/team/team_list.html'
+  })
+  .state('teamStat', {
+    url:'/teamStat',
+    templateUrl:'partials/teamstat/teamstat_list.html'
+  });
 }]);
